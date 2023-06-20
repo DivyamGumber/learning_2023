9caac2c3fa63405a719daedd54c2cfe82c428350
DivyamGumber <divyam23gumber@gmail.com>
2023-06-21 01:11:58 +0530

    day6

---
+++ Module1/day6/level_1_3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student {
+    int rollno;
+    char name[20];
+    float marks;
+};
+
+// Function to display all members of the array of structures
+void displayStudentArray(struct Student* students, int numStudents) {
+    for (int i = 0; i < numStudents; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Roll No: %d\n", students[i].rollno);
+        printf("Name: %s\n", students[i].name);
+        printf("Marks: %.2f\n", students[i].marks);
+        printf("\n");
+    }
+}
+
+int main() {
+    int numStudents;
+
+    printf("Enter the number of students: ");
+    scanf("%d", &numStudents);
+
+    struct Student* students = (struct Student*)malloc(numStudents * sizeof(struct Student));
+
+    // Assume the array of structures is initialized before calling displayStudentArray()
+
+    displayStudentArray(students, numStudents);
+
+    free(students);
+
+    return 0;
+}
