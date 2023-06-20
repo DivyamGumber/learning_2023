5fcf59486ecc49c6d4e786af71bd9dc5fec0a3f3
DivyamGumber <divyam23gumber@gmail.com>
2023-06-21 01:10:43 +0530

    day6

---
+++ Module1/day6/level_1_2.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+struct Student {
+    int rollno;
+    char name[20];
+    float marks;
+};
+
+// Function to initialize all members of the array of structures
+void initializeStudentArray(struct Student* students, int numStudents) {
+    for (int i = 0; i < numStudents; i++) {
+        students[i].rollno = 0;
+        strcpy(students[i].name, "");
+        students[i].marks = 0.0;
+    }
+}
+
+int main() {
+    int numStudents;
+
+    printf("Enter the number of students: ");
+    scanf("%d", &numStudents);
+
+    struct Student* students = (struct Student*)malloc(numStudents * sizeof(struct Student));
+
+    initializeStudentArray(students, numStudents);
+
+    // Print the initialized array of structures
+    printf("\nInitialized Student Array:\n");
+    for (int i = 0; i < numStudents; i++) {
+        printf("Student %d:\n", i + 1);
+        printf("Roll No: %d\n", students[i].rollno);
+        printf("Name: %s\n", students[i].name);
+        printf("Marks: %.2f\n", students[i].marks);
+        printf("\n");
+    }
+
+    free(students);
+
+    return 0;
+}
